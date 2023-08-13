f244817a3e0e7c39ca47149ec8602ef0949b118a
Jose Roman <el-1ero@outlook.com>
2023-08-12 21:39:38 -0400

    add splash screen

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
-import { useState } from 'react'
-import { ImageBackground, StyleSheet, SafeAreaView } from 'react-native'
+import { useState, useEffect } from 'react'
+import { ImageBackground, StyleSheet, SafeAreaView, Text } from 'react-native'
 import StartGameScreen from './screens/StartGameScreen'
 import { LinearGradient } from 'expo-linear-gradient'
 import GameScreen from './screens/GameScreen'
 import Colors from './constants/colors'
 import GameOverScreen from './screens/GameOverScreen'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync()
+	.then((result) =>
+		console.log(`SplashScreen.preventAutoHideAsync() succeeded: ${result}`)
+	)
+	.catch(console.warn)
 
 export default function App() {
 	const [userNumber, setUserNumber] = useState(null)
 	const [gameIsOver, setGameIsOver] = useState(true)
+
+	const [fontsLoaded] = useFonts({
+		'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+		'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+	})
+
+	useEffect(() => {
+		async function hideSplashScreen() {
+			await SplashScreen.hideAsync()
+		}
+		if (fontsLoaded) {
+			hideSplashScreen()
+		}
+	}, [fontsLoaded])
+
+	if (!fontsLoaded) {
+		return null
+	}
 
 	const pickedNumberHandler = (pickedNumber) => {
 		setUserNumber(pickedNumber)
